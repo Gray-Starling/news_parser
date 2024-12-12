ca0e3d72d3b1a0d7a707ca46d031851cfd5727fb
Sergey <skvorcovsa90@gmail.com>
2024-12-12 23:34:25 +1000

    edited config

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from tools.logger import setup_logger
 
 load_dotenv()
 
-SCRAPPER_SERVER_PORT = os.getenv('SERVER_PORT')
-SCRAPPER_SERVER_GET_NEWS_DATA_API_PATH = os.getenv('GET_NEWS_DATA_API_PATH')
+SERVER_PORT = os.getenv('SERVER_PORT')
+GET_NEWS_DATA_API_PATH = os.getenv('GET_NEWS_DATA_API_PATH')
 
 server_logger = setup_logger("server", "server")
 scrapper_logger = setup_logger("scrapper", "scrapper")
